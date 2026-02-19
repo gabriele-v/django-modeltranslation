8991a0eaf7fe3aace4eb53a75f8f63d76bf3de47
Gabriele-V <gabriele@villanet.it>
2026-02-19 18:12:51 +0100

    Add support for indexes

--- modeltranslation/translator.py
+++ modeltranslation/translator.py
@@ -17,7 +17,7 @@ from django.db.models import (
     OneToOneField,
     options,
 )
-from django.db.models import UniqueConstraint
+from django.db.models import Index, UniqueConstraint
 from django.db.models.base import ModelBase
 from django.db.models.signals import post_init
 from django.utils.functional import cached_property
@@ -363,6 +363,21 @@ def patch_constraints(model: type[Model], opts: TranslationOptions) -> None:
         if value := getattr(model._meta, attr_name):
             model._meta.original_attrs[attr_name] = value
 
+def patch_indexes(model: type[Model], opts: TranslationOptions) -> None:
+    def add_indexes():
+        for idx in model._meta.indexes:
+            if isinstance(idx, Index):
+                for field_name in opts.fields:
+                    if field_name in idx.fields:
+                        for translated_name in get_translation_fields(field_name):
+                            new_index = deepcopy(idx)
+                            new_fields = list(new_index.fields)
+                            new_fields[new_fields.index(field_name)] = translated_name
+                            new_index.name += f"-{translated_name}"
+                            new_index.fields = new_fields
+                            yield new_index
+
+    model._meta.indexes += list(add_indexes())  # type: ignore[operator]
 
 def delete_mt_init(sender: type[Model], instance: Model, **kwargs: Any) -> None:
     if hasattr(instance, "_mt_init"):
@@ -610,6 +625,9 @@ class Translator:
 
         # Patch constraints to correctly handle new fields
         patch_constraints(model, opts)
+        
+        # Patch indexes to correctly handle new fields
+        patch_indexes(model, opts)
 
         # Connect signal for model
         post_init.connect(delete_mt_init, sender=model)
